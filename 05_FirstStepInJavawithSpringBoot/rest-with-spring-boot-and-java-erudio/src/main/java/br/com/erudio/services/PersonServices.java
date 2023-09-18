f359cf603329121600584d1b2879031146964eb1
mateusteixeir <mateusteixeiradas@gmail.com>
2023-09-18 17:26:39 -0300

    parte 3

---
+++ 05_FirstStepInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -0,0 +1,29 @@
+package br.com.erudio.services;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.model.Person;
+
+@Service
+public class PersonServices {
+	
+
+	private final AtomicLong counter = new AtomicLong();
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+	
+	public Person findById(String id) {
+		
+		logger.info("Finding one person!");
+		
+		Person person =  new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Mateus");
+		person.setLastName("Soares");
+		person.setAddress("Fortaleza - Ceara - Brasil");
+		person.setGender("Male");
+		return person;
+	}
+}
